86623d4e4754d40b9ad9d7c89b377a20eea7dfa4
Joseph Bernshine <bernshi3@myumanitoba.ca>
2015-11-30 20:10:38 -0600

    More AdvancedSearch and SearchQuery stuff. Certification is todo

--- Comp3020A3/AdvancedSearchForm.cs
+++ Comp3020A3/AdvancedSearchForm.cs
@@ -90,5 +90,63 @@ namespace Comp3020A3
                 numericLength2.Value = numericLength1.Value;
             }
         }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            SearchQuery query = new SearchQuery();
+
+            query.title = txtTitle.Text.Trim();
+
+            query.action = chkAction.Checked;
+            query.comedy = chkComedy.Checked;
+            query.family = chkFamily.Checked;
+            query.history = chkHistory.Checked;
+            query.mystery = chkMystery.Checked;
+            query.scifi = chkSciFi.Checked;
+            query.war = chkWar.Checked;
+            query.adventure = chkAdventure.Checked;
+            query.crime = chkCrime.Checked;
+            query.fantasy = chkFantasy.Checked;
+            query.horror = chkHorror.Checked;
+            query.news = chkNews.Checked;
+            query.sport = chkSport.Checked;
+            query.western = chkWestern.Checked;
+            query.animation = chkAnimation.Checked;
+            query.documentary = chkDocumentary.Checked;
+            query.filmnoir = chkFilmNoir.Checked;
+            query.music = chkMusic.Checked;
+            query.realitytv = chkRealityTV.Checked;
+            query.talkshow = chkTalkShow.Checked;
+            query.biography = chkBiography.Checked;
+            query.drama = chkDrama.Checked;
+            query.gameshow = chkGameShow.Checked;
+            query.musical = chkMusical.Checked;
+            query.romance = chkRomance.Checked;
+            query.thriller = chkThriller.Checked;
+
+            query.director = txtDirector.Text.Trim();
+
+            foreach (TextBox actor in textActors)
+            {
+                query.actors.Add(actor.Text.Trim());
+            }
+
+            query.g = chkG.Checked;
+            query.pg = chkPG.Checked;
+            query.pg13 = chkPG13.Checked;
+            query.r = chkR.Checked;
+            query.nc17 = chkNC17.Checked;
+
+            query.rating1 = (int)numericRating1.Value;
+            query.rating2 = (int)numericRating2.Value;
+
+            query.year1 = (int)numericYear1.Value;
+            query.year2 = (int)numericYear2.Value;
+
+            query.length1 = (int)numericLength1.Value;
+            query.length2 = (int)numericLength2.Value;
+
+            query.Search();
+        }
     }
 }
