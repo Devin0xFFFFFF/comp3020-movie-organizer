5226dc86886e774d45be964feeb1652698d1fe00
Joseph Bernshine <bernshi3@myumanitoba.ca>
2015-12-03 00:02:02 -0600

    Forgot the other part

--- Comp3020A3/SearchQuery.cs
+++ Comp3020A3/SearchQuery.cs
@@ -68,7 +68,7 @@ namespace Comp3020A3
             {
                 bool deleted = false;
 
-                if (title != "" && !movies.ElementAt(i).title.ToLower().Contains(title)) { movies.RemoveAt(i); deleted = true; }
+                if (title != "" && !movies.ElementAt(i).title.ToLower().Contains(title.ToLower())) { movies.RemoveAt(i); deleted = true; }
 
                 if (!deleted && checkGenres(movies.ElementAt(i)) == false) { movies.RemoveAt(i); deleted = true; }
 
@@ -102,7 +102,7 @@ namespace Comp3020A3
                 */
 
 
-                if (!deleted && director != "" && !movies.ElementAt(i).director.ToLower().Contains(director)) { movies.RemoveAt(i); deleted = true; }
+                if (!deleted && director != "" && !movies.ElementAt(i).director.ToLower().Contains(director.ToLower())) { movies.RemoveAt(i); deleted = true; }
 
 
                 if (!deleted && checkActors(movies.ElementAt(i)) == false) { movies.RemoveAt(i); deleted = true; }
@@ -152,7 +152,7 @@ namespace Comp3020A3
                 foreach (String actor in actors)
                 {
                     // If a user inputed actor is contained in the movie, accept
-                    if (actor.Equals("") && movieActor.ToLower().Contains(actor))
+                    if (actor.Equals("") && movieActor.ToLower().Contains(actor.ToLower()))
                     {
                         match = true;
                         break;
